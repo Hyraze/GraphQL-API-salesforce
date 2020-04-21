8f5dca49882dced7169e4ff084de8166e0a3f621
Hanishraj B Rao <hanishrao321@gmail.com>
2020-04-22 01:26:27 +0530

    seed

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,37 +1,37 @@
 'use strict'
 
 const resolvers = {
-    Query:{
-        helloWorld(){
-            return 'Hello!'
-        },
-        listCustomers (_,{ limit }, ctx) {
-            return ctx.db.customer.findAll({ limit })
-        },
-        listProperties (_, { limit }, ctx) {
-            return ctx.db.property.findAll({
-                limit,
-                where: {
-                    approved: true
-                }
-            })
-        },
-
-        findProperties (_, { city, bathrooms , bedrooms, limit}, ctx) {
-            const where = {
-                ...(city != null && {city}),
-                ...(bathrooms!= null && {bathrooms}),
-                ...(bedrooms!= null && { bedrooms}),
-                approved: true
-            }
-            return ctx.db.property.findAll({
-                where,
-                limit,
-                order: [
-                    ['id','DESC']
-                ]
-            })
+  Query: {
+    helloWorld () {
+      return 'Hello!'
+    },
+    listCustomers (_, { limit }, ctx) {
+      return ctx.db.customer.findAll({ limit })
+    },
+    listProperties (_, { limit }, ctx) {
+      return ctx.db.property.findAll({
+        limit,
+        where: {
+          approved: true
         }
-
-     }
+      })
+    },
+    findProperties (_, { city, bathrooms, bedrooms, limit }, ctx) {
+      const where = {
+        ...(city != null && { city }),
+        ...(bathrooms != null && { bathrooms }),
+        ...(bedrooms != null && { bedrooms }),
+        approved: true
+      }
+      return ctx.db.property.findAll({
+        where,
+        limit,
+        order: [
+          ['id', 'DESC']
+        ]
+      })
+    }
+  }
 }
+
+module.exports = resolvers
