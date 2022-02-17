7e9d54094969b4278caf93ebd097a938a7db7c9f
hanishraj <hanish@protonmail.com>
2022-02-17 21:37:52 +0530

    replaced faker with @ngneat/falso

--- seeders/20202604-create-property.js
+++ seeders/20202604-create-property.js
@@ -1,21 +1,22 @@
 'use strict'
 
-const faker = require('faker')
+import faker from '@ngneat/falso';
+
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     const properties = []
     for (let i = 0; i < 3e3; i++) {
       properties.push({
-        address: faker.address.streetAddress(),
-        city: faker.address.city(),
-        state: faker.address.stateAbbr(),
-        zipCode: faker.address.zipCode(),
-        country: 'United States',
-        bedrooms: faker.random.number(5),
-        bathrooms: faker.random.number(5),
-        area: faker.random.number(3000),
-        description: faker.random.words(5),
+        address: faker.randAddress(),
+        city: faker.randCity(),
+        state: faker.randState(),
+        zipCode: faker.randZipCode(),
+        country: faker.randCountry(),
+        bedrooms: faker.randText({ charCount: 5 }),
+        bathrooms: faker.randText({ charCount: 5 }),
+        area: faker.randNumber(3000),
+        description: faker.randWord(),
         approved: true,
         createdAt: new Date(),
         updatedAt: new Date()
